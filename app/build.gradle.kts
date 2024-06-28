69b095bee18864dc50beb5a650a333154606d6d7
James <jamesfangyauw@gmail.com>
2024-06-28 22:10:42 +0800

    try to fix error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.james.challenge4"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
